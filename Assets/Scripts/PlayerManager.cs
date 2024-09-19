be57c0690819f180f0a0ebac2fa4c744d4e69742
Taihou F. <ansonfeng1999@gmail.com>
2024-09-19 12:07:41 -0600

    Fixing rotation angle

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,13 +5,26 @@ public class PlayerManager : MonoBehaviour
 {
     [Header("Attributes")]
     public float playerRotateSpeed;
+    [SerializeField]
+    private float vesselRotateSpeed;
+    [Tooltip("Tells Whether the player is in control")]
+    [SerializeField]
+    private bool controlling;
+    [SerializeField]
+    private bool hasDirection;
+    [SerializeField]
+    private int direction;
+    private int[] dir = { -1, 1 };
+    [SerializeField]
+    private float maxY;
+    [SerializeField]
+    private float minY;
     private float horizontalInput;
     private Rigidbody rb;
 
     [Tooltip("Abs of the angle that the player must be less than on the Y axis to count as charging since they'll be facing the lighthouse")]
     [SerializeField]
     private float angleCheck;
-    // Start is called before the first frame update
 
     [Tooltip("The value that determines in seconds how long the player has to look at the lighthouse before being prompted to answer a dialogue")]
     [SerializeField]
@@ -32,6 +45,28 @@ public class PlayerManager : MonoBehaviour
         dialogueManager = FindObjectOfType<DialogueManager>();
         gameManager = FindObjectOfType<GameManager>();
         lightHouseManager = FindObjectOfType<LightHouseManager>();
+
+        controlling = false;
+        hasDirection = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontalInput != 0) 
+        {
+            controlling = true;
+            hasDirection = false;
+        }
+        else if (!controlling && !hasDirection)
+        {
+            hasDirection = true;
+            var rng = Random.Range(0, 2);
+            direction = dir[rng];
+        }
+        else
+        {
+            controlling = false;
+        }
     }
 
     // Update is called once per frame
@@ -40,6 +75,7 @@ public class PlayerManager : MonoBehaviour
         //Only update the rotation if the game state is active
         if (gameManager.GetGameState() == GameState.ACTIVE)
         {
+            horizontalInput = Input.GetAxis("Horizontal");
             UpdatePlayerRotation();
 
             CheckCharge();
@@ -52,8 +88,26 @@ public class PlayerManager : MonoBehaviour
     /// </summary>
     private void UpdatePlayerRotation() 
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, playerRotateSpeed * horizontalInput * Time.deltaTime);
+        Vector3 currRotation = transform.rotation;
+        if (horizontalInput != 0)
+        {
+            //Debug.Log(horizontalInput);
+            Debug.Log(transform.eulerAngles.y);
+            if ((horizontalInput < 0 && transform.eulerAngles.y <= minY) || (horizontalInput > 0 && transform.eulerAngles.y >= maxY)) return;
+            //Vector3 currRotation = transform.rotation;
+
+            transform.Rotate(Vector3.up, playerRotateSpeed * horizontalInput * Time.deltaTime);
+        }
+        else if (!controlling)
+        {
+            Debug.Log(transform.eulerAngles.y);
+            if ((direction < 0 && transform.eulerAngles.y <= 360 - maxY) || (direction > 0 && transform.eulerAngles.y >= maxY))
+            {
+                Debug.Log("Out of Bounds now");
+                return; 
+            }
+            transform.Rotate(Vector3.up, vesselRotateSpeed * direction * Time.deltaTime);
+        }
     }
 
     private void CheckCharge()
